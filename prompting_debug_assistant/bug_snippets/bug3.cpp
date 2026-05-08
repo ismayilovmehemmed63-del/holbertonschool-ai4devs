eeb51b22724e1fedeb2c1230a1f6aa19904ec0fd
Məhəmməd <ismayilovmehemmed63@gmail.com>
2026-05-08 14:54:14 +0400

    Update bug3.cpp

--- prompting_debug_assistant/bug_snippets/bug3.cpp
+++ prompting_debug_assistant/bug_snippets/bug3.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
-
 int* createArray(int size) {
-    // Xəta: Lokal massiv funksiya bitəndə silinir (Dangling Pointer)
     int arr[size]; 
     for(int i = 0; i < size; i++) {
         arr[i] = i * 10;
@@ -12,14 +10,10 @@ int* createArray(int size) {
 
 int main() {
     int n = 5;
-    int* myPtr = createArray(n);
-    
+    int* myPtr = createArray(n);  
     std::cout << "Array elements: " << std::endl;
-    
-    // Xəta: i <= n olması "Out of bounds" xətası yaradır
     for(int i = 0; i <= n; i++) {
         std::cout << "Index " << i << ": " << myPtr[i] << std::endl;
     }
-    
     return 0;
 }
